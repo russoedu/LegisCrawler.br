ea1a3979f55d4c964bf74fbe00b73665a54e4f52
Eduardo Russo <russoedu@gmail.com>
2016-11-22 08:41:54 -0200

    [master] Updated error helper

--- helpers/error.js
+++ helpers/error.js
@@ -1,8 +1,10 @@
-const log = require('better-log').setConfig({ depth: 8 });
+const log = require('better-log').setConfig({
+  depth: 8,
+});
 const chalk = require('chalk');
 
 module.exports = function error(type, message, data) {
-  log.error(chalk.red(`⛔️   [ERROR] ${message}: ${message}`));
+  log.error(chalk.red(`⛔️   [ERROR] ${type}: ${message}`));
   log.error(chalk.red('#### error message start ####'));
   log.error(data);
   log.error(chalk.red('#### error message end ######'));
