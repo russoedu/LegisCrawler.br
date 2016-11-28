26331286c8394d3dacabb99ecd6f4df039c2df73
Eduardo Russo <russoedu@gmail.com>
2016-11-28 19:01:43 -0200

    [develop] Added pmx module

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const pmx = require('pmx').init({
+  http: true, // HTTP routes logging (default: true)
+  errors: true, // Exceptions loggin (default: true)
+  custom_probes: true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network: true, // Network monitoring at the application level
+});
 const http = require('http');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
